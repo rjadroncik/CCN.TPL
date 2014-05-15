fb62276ee550043d5fcc0378cf23b4165fde3dee
Roman Jadroncik <rjadroncik@sportingindex.com>
2014-05-15 16:45:08 +0100

    - added: Connector service to easily construct connectors and connections

---
+++ CCNUtils/DBWrangler/DBWrangler/Services/Connector.vb
@@ -0,0 +1,69 @@
+﻿Imports Npgsql
+Imports Oracle.DataAccess.Client
+Imports DBWrangler.Connectors
+Imports DBWrangler.Enums
+Imports System.Data.SqlClient
+
+Namespace Services
+
+    Public Class Connector
+
+        Public Shared Function Create(vendor As DatabaseVendor, connectionString As String) As IConnector
+
+            Select (vendor)
+                Case DatabaseVendor.Oracle
+
+                    Dim connection As New OracleConnection(connectionString)
+                    connection.Open()
+
+                    Return New OracleConnector(connection)
+
+                Case DatabaseVendor.Postgre
+
+                    Dim connection As New NpgsqlConnection(connectionString)
+                    connection.Open()
+
+                    Return New PgConnector(connection)
+
+                Case DatabaseVendor.Microsoft
+
+                    Dim connection As New SqlConnection(connectionString)
+                    connection.Open()
+
+                    Return New MsConnector(connection)
+            End Select
+
+            Throw New ArgumentException()
+        End Function
+
+        Public Shared Function CreateConnection(vendor As DatabaseVendor, connectionString As String) As IDbConnection
+
+            Select Case (vendor)
+                Case DatabaseVendor.Oracle
+
+                    Dim connection As New OracleConnection(connectionString)
+                    connection.Open()
+
+                    Return connection
+
+                Case DatabaseVendor.Postgre
+
+                    Dim connection As New NpgsqlConnection(connectionString)
+                    connection.Open()
+
+                    Return connection
+
+                Case DatabaseVendor.Microsoft
+
+                    Dim connection As New SqlConnection(connectionString)
+                    connection.Open()
+
+                    Return connection
+            End Select
+
+            Throw New ArgumentException()
+        End Function
+
+    End Class
+
+End Namespace
